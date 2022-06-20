d0bfaeae9dee10a5d0633f19e8aa2ee945a7d3f4
giangdeptrai <giangtong199@gmail.com>
2022-06-20 15:55:49 +0700

    solution

--- src/teacherclasses/Data.java
+++ src/teacherclasses/Data.java
@@ -18,6 +18,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
  * @author saplab
  */
 public class Data {
+
     int L; //number of subjects
     int M; //number of courses
     int N; //number of teachers
@@ -28,28 +29,42 @@ public class Data {
     Teacher[] teachers = new Teacher[500]; //list of teachers
     Course[] courses = new Course[500];
     
+    double w1; // weight of total rating 
+    double w2; // weight of total salary
+    double w3; // weight of teacher favour slot
+    double w4; // weight of teacher favour subject
+    double w5; // weight of teacher error slot
+    double w6; // weight of teacher periods
+    double c1; // Weight of P_0
+    double c2; // weight of P_j
     public Data() {
-        
+
     }
-    
-    public static Data readDataFromFile() throws FileNotFoundException, IOException{
+
+    public static Data readDataFromFile() throws FileNotFoundException, IOException {
         Data data = new Data();
         data.L = 13;
         data.M = 153;
         data.N = 25;
         data.T = 10;
-        
+        data.w1 = 1;
+        data.w2 = 1;
+        data.w3 = 1;
+        data.w4 = 1;
+        data.w5 = 1;
+        data.w6 = 1;
+
         String workingDirectory = System.getProperty("user.dir");
         String excelFilePath = workingDirectory + "//data//data_T.xlsx";
-        
+
         InputStream inputStream = new FileInputStream(new File(excelFilePath));
 
         Workbook workbook = new XSSFWorkbook(inputStream);
         Sheet sheet = (Sheet) workbook.getSheetAt(0);
-        
+
         for (int i = 1; i <= data.N; i++) {
             Teacher t = new Teacher();
-            t.setId(i-1);
+            t.setId(i - 1);
             t.setName(sheet.getRow(i).getCell(1).toString());
             t.setSalaryLevel((double) sheet.getRow(i).getCell(2).getNumericCellValue());
             t.setMinClass((int) sheet.getRow(i).getCell(3).getNumericCellValue());
@@ -57,55 +72,55 @@ public class Data {
             t.setIdealClass((int) sheet.getRow(i).getCell(5).getNumericCellValue());
             data.teachers[i - 1] = t;
         }
-        
+
         excelFilePath = workingDirectory + "//data//data_FSlot_S.xlsx";
         inputStream = new FileInputStream(new File(excelFilePath));
         workbook = new XSSFWorkbook(inputStream);
         sheet = (Sheet) workbook.getSheetAt(0);
-        
-        for (int i = 0; i< data.N; i++){
-            for(int j = 0; j < data.L; j++){
+
+        for (int i = 0; i < data.N; i++) {
+            for (int j = 0; j < data.L; j++) {
                 data.FSlot[i][j] = sheet.getRow(i + 1).getCell(j + 1).getNumericCellValue();
             }
         }
-        
+
         excelFilePath = workingDirectory + "//data//data_FSlot_T.xlsx";
         inputStream = new FileInputStream(new File(excelFilePath));
         workbook = new XSSFWorkbook(inputStream);
         sheet = (Sheet) workbook.getSheetAt(0);
-        
-        for (int i = 0; i< data.N; i++){
-            for(int j = 0; j < data.T; j++){
+
+        for (int i = 0; i < data.N; i++) {
+            for (int j = 0; j < data.T; j++) {
                 data.FSlot[i][j] = sheet.getRow(i + 1).getCell(j + 1).getNumericCellValue();
             }
         }
-        
+
         excelFilePath = workingDirectory + "//data//data_Rating.xlsx";
         inputStream = new FileInputStream(new File(excelFilePath));
         workbook = new XSSFWorkbook(inputStream);
         sheet = (Sheet) workbook.getSheetAt(0);
-        
-        for (int i = 0; i< data.N; i++){
-            for(int j = 0; j < data.L; j++){
+
+        for (int i = 0; i < data.N; i++) {
+            for (int j = 0; j < data.L; j++) {
                 data.Rating[i][j] = sheet.getRow(i + 1).getCell(j + 1).getNumericCellValue();
             }
         }
-        
+
         excelFilePath = workingDirectory + "//data//sp22_to_import.xlsx";
         inputStream = new FileInputStream(new File(excelFilePath));
         workbook = new XSSFWorkbook(inputStream);
         sheet = (Sheet) workbook.getSheetAt(0);
         for (int i = 1; i <= data.M; i++) {
             Course c = new Course();
-            c.setId(i-1);
+            c.setId(i - 1);
             c.setClasses(sheet.getRow(i).getCell(1).toString());
             c.setSubject((int) sheet.getRow(i).getCell(3).getNumericCellValue());
             c.setSlot((int) sheet.getRow(i).getCell(5).getNumericCellValue());
             c.setRoom(sheet.getRow(i).getCell(7).toString());
             data.courses[i - 1] = c;
         }
-        
+
         return data;
     }
-    
-} 
+
+}
--- src/teacherclasses/Solution.java
+++ src/teacherclasses/Solution.java
@@ -4,11 +4,16 @@
  */
 package teacherclasses;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Vector;
+
 /**
  *
  * @author saplab
  */
 public class Solution {
+
     public int chromosome[][];
     public Data data;
 
@@ -19,9 +24,147 @@ public class Solution {
     public void setChromosome(int[][] chromosome) {
         this.chromosome = chromosome;
     }
-    
-    public Solution(Data data){
+
+    public Solution(Data data) {
         this.data = data;
+    }
+
+    public double cal_Quality_P0(Data data) {
+        double quality = 0;
+        for (int i = 0; i < data.M; i++) {
+            for (int j = 0; j < data.N; j++) {
+                quality += chromosome[i][j] * data.Rating[i][j];
+            }
+        }
+        return quality;
+    }
+
+    public double cal_Salary_P0(Data data) {
+        double salary = 0;
+        for (int i = 0; i < data.M; i++) {
+            for (int j = 0; j < data.N; j++) {
+                salary += chromosome[i][j] * data.teachers[j].getSalaryLevel();
+            }
+        }
+        return salary;
+    }
+
+    public double cal_Payoff_P0(Data data) {
+        double payoff;
+        payoff = data.w1 * cal_Quality_P0(data) / (data.w2 * cal_Salary_P0(data));
+        return payoff;
+    }
+
+    public double cal_Favourite_Subs_PJ(Data data, int teacher) {
+        double favourite_Subs = 0;
+        for (int i = 0; i < data.M; i++) {
+            favourite_Subs += chromosome[i][teacher] * data.FSub[i][teacher];
+        }
+        return favourite_Subs;
+    }
+    
+    public double cal_Favourite_Slots_PJ(Data data, int teacher) {
+        double favourite_Slots = 0;
+        for (int i = 0; i < data.M; i++) {
+            favourite_Slots += chromosome[i][teacher] * data.FSlot[i][teacher];
+        }
+        return favourite_Slots;
+    }
+    public double cal_Err_Courses_PJ(Data data,int teacher){
+        double Err_Courses = 0;
+        for(int i=0;i<data.M;i++){
+            Err_Courses += chromosome[i][teacher];
+        }
+        Err_Courses = data.teachers[teacher].getIdealClass()-Err_Courses;
+        return  Math.abs(Err_Courses);
+    }
+    public double cal_Periods_PJ(Data data,int teacher){
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=0;i<data.M;i++){
+           if(chromosome[i][teacher] == 1) {
+               list.add(data.courses[i].getSlot());
+           }      
+        }
+        Collections.sort(list);
+        int t = 1;
+        for(int i = 1 ; i < list.size() ; i++) {
+            if (list.get(i) - 1 != list.get(i-1)) 
+                t++;
+        }
+        
+        return t;
+       
+    }
+    public double cal_Payoff_PJ(Data data,int teacher){
+        double payoff =(data.w3*cal_Favourite_Subs_PJ(data, teacher)+data.w4*cal_Favourite_Slots_PJ(data, teacher))/(data.w5*cal_Err_Courses_PJ(data, teacher)+data.w6*cal_Periods_PJ(data, teacher));
+        return payoff;
+    }
+    public double cal_Payoff_All_PJ(Data data){
+        double payoff=0;
+        for(int i=0;i<data.N;i++){
+            payoff+=cal_Payoff_PJ(data, i);
+        }
+        return  payoff;
+    }
+    
+    public double cal_Fitness(Data data){
+        return data.c1*cal_Payoff_P0(data)+data.c2*cal_Payoff_All_PJ(data);
+    }
+    
+    public boolean check_Solution(Data data){
+        // all courses have a teacher assigned
+        int sumSlot = 0;
+        for (int i = 0; i < data.M; i++) {
+            for (int j = 0; j < data.N; j++) {
+                sumSlot += chromosome[i][j];
+            }
+        }
+        
+        if (sumSlot != data.M)
+            return false;
+        
+        // no courses have 2 teachers
+        for (int i = 0; i < data.M; i++) {
+            int sum2 = 0;
+            for (int j = 0; j < data.N; j++) {
+                sum2 += chromosome[i][j];
+            }
+            if (sum2 != 1) return false;
+            sum2 = 0;
+        }
+        
+        // no teachers teach 2 different courses in the same slot
+        for (int i = 0; i<data.N; i++){
+            ArrayList<Integer> list = new ArrayList<>();
+            for (int j = 0; j < data.M; j++) {
+                if(chromosome[j][i] == 1) {
+                    list.add(data.courses[j].getSlot());
+                }  
+            }
+            Collections.sort(list);
+        
+            for(int j = 1 ; j < list.size() ; j++) {
+                if (list.get(j) == list.get(j-1)) 
+                    return false;
+            }
+            list.clear();
+        }
+        
+        //total number of slots of a teacher within min and max specified
+        for (int i = 0; i< data.N; i++){
+            int sum3 = 0;
+            for (int j = 0; j < data.M; i++){
+                sum3 += chromosome[j][i];
+            }
+            if (sum3 > data.teachers[i].getMaxClass() || sum3 < data.teachers[i].getMinClass()){
+                return false;
+            }
+        }
+        
+        
+        
+        return true;
     }
     
 }
